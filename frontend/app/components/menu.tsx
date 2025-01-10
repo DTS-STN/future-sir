824ff9cd17e2b05fbdd754d5c40b9ff27db40b71
Dario Au <dario.au@hrsdc-rhdcc.gc.ca>
2025-01-10 03:45:01 -0700

    feat(frontend): add user button and dropdown menu component (#66)
    * Updated Menu Component
    
    * feat(frontend): add user button and dropdown menu component
    
    - Added Shadcn Dropdown Menu
    - Updated Menu Component
    - Added User Button for profile information and logout
    
    * pr fixes
    
    ---------
    
    Co-authored-by: Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>

--- frontend/app/components/menu.tsx
+++ frontend/app/components/menu.tsx
@@ -3,22 +3,24 @@ import { useState } from 'react';
 
 import { useTranslation } from 'react-i18next';
 
-import { InlineLink } from './inline-link';
-
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '~/components/dropdown-menu';
+import { InlineLink } from '~/components/inline-link';
 import { cn } from '~/utils/tailwind-utils';
 
 type MenuItemProps = ComponentProps<typeof InlineLink>;
 
 export function MenuItem({ children, ...props }: MenuItemProps) {
   return (
-    <InlineLink
-      role="menuitem"
-      id="menu-item"
-      className="text-md text-md text-md block px-4 py-2 text-white hover:bg-slate-300 hover:text-blue-950 focus:bg-slate-600 focus:text-blue-400 active:bg-slate-800 active:text-white"
-      {...props}
-    >
-      {children}
-    </InlineLink>
+    <DropdownMenuItem className="text-md m-0 p-0 hover:!bg-slate-700">
+      <InlineLink
+        role="menuitem"
+        id="menu-item"
+        className="w-full px-3 py-2 text-white hover:bg-slate-300 hover:text-blue-950 focus:bg-slate-600 focus:text-white active:bg-slate-800 active:text-white"
+        {...props}
+      >
+        {children}
+      </InlineLink>
+    </DropdownMenuItem>
   );
 }
 
@@ -31,63 +33,39 @@ export function Menu({ className, children }: MenuProps) {
   const { t } = useTranslation(['gcweb']);
   const [open, setOpen] = useState(false);
   const baseClassName = cn(
-    'hover:underline text-lg inline-flex justify-center space-x-2 rounded-b-md border-b border-l border-r border-slate-700 px-4 py-2 ring-black ring-opacity-5',
+    'focus:outline-none hover:underline select-none text-lg inline-flex justify-center space-x-2 rounded-b-md border-b border-l border-r border-slate-700 px-4 py-2 ring-black ring-opacity-5',
     open ? 'bg-slate-900 text-white hover:bg-slate-800' : 'bg-slate-700 text-white hover:bg-slate-600',
   );
 
-  const onClick = () => {
-    setOpen((value) => !value);
-  };
-
   return (
-    <div className="relative inline-block text-left">
-      <button onClick={onClick} className={cn(baseClassName, className)} aria-haspopup={true} aria-expanded={open}>
-        <span>{t('gcweb:app.menu')}</span>
-        {open ? <UpChevron /> : <DownChevron />}
-      </button>
-      {open && (
-        <div
-          className="absolute left-0 mt-2 w-64 origin-top-left rounded-md bg-slate-700 shadow-lg ring-1 ring-black ring-opacity-5"
-          role="menu"
-          aria-orientation="vertical"
-          aria-labelledby="menu-button"
-          tabIndex={-1}
-        >
-          <div className="py-1" role="none">
-            {children}
-          </div>
-        </div>
-      )}
-    </div>
+    <DropdownMenu onOpenChange={(open) => setOpen(open)}>
+      <DropdownMenuTrigger asChild className={cn(baseClassName, className)} aria-haspopup={true} aria-expanded={open}>
+        <button>
+          <span id="menu-label">{t('gcweb:app.menu')}</span>
+          {open ? <UpChevron /> : <DownChevron />}
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="start" className="w-64 !bg-slate-700">
+        {children}
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
 
 function UpChevron() {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="size-6"
-    >
-      <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="my-auto size-5 fill-current">
+      {/*!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.*/}
+      <path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z" />
     </svg>
   );
 }
 
 function DownChevron() {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="my-auto size-6"
-    >
-      <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="my-auto size-5 fill-current">
+      {/*!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.*/}
+      <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
     </svg>
   );
 }
---
+++ frontend/app/components/user-button.tsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+
+import { useTranslation } from 'react-i18next';
+
+import { DropdownMenu, DropdownMenuContent, DropdownMenuLabel, DropdownMenuTrigger } from '~/components/dropdown-menu';
+import { MenuItem } from '~/components/menu';
+import { cn } from '~/utils/tailwind-utils';
+
+interface UserButtonProps {
+  className?: string;
+  children?: React.ReactNode;
+  name?: string;
+}
+
+export function UserButton({ className, children, name }: UserButtonProps) {
+  const [open, setOpen] = useState(false);
+  const { t } = useTranslation(['gcweb']);
+  const baseClassName = cn(
+    'w-10 h-10 my-0 text-white rounded-full flex items-center justify-center focus:outline-none focus:ring-2 focus:ring-slate-300',
+    open ? 'bg-slate-900 hover:bg-slate-800' : 'bg-slate-700 hover:bg-slate-600',
+  );
+
+  return (
+    <DropdownMenu onOpenChange={(open) => setOpen(open)}>
+      <DropdownMenuTrigger
+        asChild
+        title={t('gcweb:app.profile')}
+        aria-label={t('gcweb:app.profile')}
+        className={cn(baseClassName, className)}
+        aria-haspopup={true}
+        aria-expanded={open}
+      >
+        <button>
+          <UserCircleIcon />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-64 !bg-slate-700">
+        <UserName name={name} />
+        {children}
+        <MenuItem to="/auth/logout">{t('gcweb:app.logout')}</MenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+function UserCircleIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="currentColor" className="size-6">
+      {/*!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.*/}
+      <path d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464l349.5 0c-8.9-63.3-63.3-112-129-112l-91.4 0c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304l91.4 0C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7L29.7 512C13.3 512 0 498.7 0 482.3z" />
+    </svg>
+  );
+}
+
+function UserName({ name }: { name?: string }) {
+  return (
+    <>
+      {name !== undefined && (
+        <DropdownMenuLabel className="text-md flex items-center border-b-2 border-slate-600 px-3 py-2 text-gray-300">
+          <UserIcon />
+          {name}
+        </DropdownMenuLabel>
+      )}
+    </>
+  );
+}
+
+function UserIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="currentColor" className="mr-2 size-4">
+      {/*!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.*/}
+      <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304l-91.4 0z" />
+    </svg>
+  );
+}
