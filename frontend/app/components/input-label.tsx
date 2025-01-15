5d84a77bf7649e2d2168a76629618bd989e829f3
faiza-jahanzeb <95882212+faiza-jahanzeb@users.noreply.github.com>
2025-01-15 10:02:35 -0500

    components for Text field (#90)

---
+++ frontend/app/components/input-label.tsx
@@ -0,0 +1,18 @@
+import type { ComponentProps, ReactNode } from 'react';
+
+import { cn } from '~/utils/tailwind-utils';
+
+export interface InputLabelProps extends ComponentProps<'label'> {
+  children: ReactNode;
+  id: string;
+}
+
+export function InputLabel(props: InputLabelProps) {
+  const { children, className, ...restProps } = props;
+
+  return (
+    <label className={cn('inline-block font-semibold', className)} data-testid="input-label" {...restProps}>
+      <span>{children}</span>
+    </label>
+  );
+}
