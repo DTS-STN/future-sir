7b51001b5ee501b9d148bd4bb6fb86fde56cf1fe
faiza-jahanzeb <95882212+faiza-jahanzeb@users.noreply.github.com>
2025-01-24 14:55:09 -0500

    add component: input checkbox (#124)

---
+++ frontend/app/components/input-legend.tsx
@@ -0,0 +1,17 @@
+import type { ComponentProps, ReactNode } from 'react';
+
+import { cn } from '~/utils/tailwind-utils';
+
+export interface InputLegendProps extends ComponentProps<'legend'> {
+  children: ReactNode;
+}
+
+export function InputLegend(props: InputLegendProps) {
+  const { children, className, ...restProps } = props;
+
+  return (
+    <legend className={cn('block font-semibold', className)} data-testid="input-legend" {...restProps}>
+      <span>{children}</span>
+    </legend>
+  );
+}
