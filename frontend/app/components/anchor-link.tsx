9cacf4817cc6cd65b3544581456b8d8ac07f7c19
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2025-01-26 09:46:32 -0330

    test(frontend): increase test coverage

--- frontend/app/components/anchor-link.tsx
+++ frontend/app/components/anchor-link.tsx
@@ -1,7 +1,5 @@
 import type { ComponentProps, MouseEvent } from 'react';
 
-import { scrollAndFocusFromAnchorLink } from '~/utils/link-utils';
-
 /**
  * AnchorLinkProps represents the properties for the AnchorLink component.
  * It extends the ComponentProps<'a'> type, omitting the 'href' property,
@@ -15,22 +13,20 @@ export interface AnchorLinkProps extends OmitStrict<ComponentProps<'a'>, 'href'>
  * AnchorLink is a React component used to create anchor links that scroll
  * and focus on the specified target element when clicked.
  *
- * @param {AnchorLinkProps} props - The properties for the AnchorLink component.
- * @returns {JSX.Element} JSX element representing the anchor link.
+ * @param props - The properties for the AnchorLink component.
+ * @returns React element representing the anchor link.
  */
-export function AnchorLink(props: AnchorLinkProps) {
-  const { anchorElementId, children, onClick, ...restProps } = props;
-
+export function AnchorLink({ anchorElementId, children, onClick, ...restProps }: AnchorLinkProps): React.ReactElement {
   /**
    * handleOnSkipLinkClick is the click event handler for the anchor link.
    * It prevents the default anchor link behavior, scrolls to and focuses
    * on the target element specified by 'anchorElementId', and invokes
    * the optional 'onClick' callback.
    */
-  function handleOnSkipLinkClick(e: MouseEvent<HTMLAnchorElement>) {
-    e.preventDefault();
-    scrollAndFocusFromAnchorLink(e.currentTarget.href);
-    onClick?.(e);
+  function handleOnSkipLinkClick(event: MouseEvent<HTMLAnchorElement>) {
+    event.preventDefault();
+    scrollAndFocusFromAnchorLink(event.currentTarget.href);
+    onClick?.(event);
   }
 
   return (
@@ -38,4 +34,24 @@ export function AnchorLink(props: AnchorLinkProps) {
       {children}
     </a>
   );
+}
+
+/**
+ * Scrolls and focuses on the element identified by the anchor link's hash.
+ *
+ * @param href - The anchor link URL.
+ */
+function scrollAndFocusFromAnchorLink(href: string): void {
+  if (URL.canParse(href)) {
+    const { hash } = new URL(href);
+
+    if (hash) {
+      const targetElement = document.querySelector<HTMLElement>(hash);
+
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+        targetElement.focus();
+      }
+    }
+  }
 }
--- frontend/tests/components/anchor-link.test.tsx
+++ frontend/tests/components/anchor-link.test.tsx
@@ -1,11 +1,57 @@
-import { render } from '@testing-library/react';
-import { describe, expect, it } from 'vitest';
+import { fireEvent, render } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
 
 import { AnchorLink } from '~/components/anchor-link';
 
 describe('AnchorLink', () => {
   it('should render anchor link component', () => {
-    const { container } = render(<AnchorLink anchorElementId="id">input test</AnchorLink>);
+    const { container } = render(
+      <>
+        <AnchorLink anchorElementId="id" onClick={vi.fn()}>
+          click here
+        </AnchorLink>
+        <div id="id">Some content.</div>
+      </>,
+    );
+
     expect(container.innerHTML).toMatchSnapshot('expected html');
+  });
+
+  it('should scroll to the correct element', () => {
+    HTMLElement.prototype.scrollIntoView = vi.fn();
+    HTMLElement.prototype.focus = vi.fn();
+
+    const { container, getByTestId } = render(
+      <>
+        <AnchorLink anchorElementId="id" onClick={vi.fn()}>
+          click here
+        </AnchorLink>
+        <div id="id">Some content.</div>
+      </>,
+    );
+
+    fireEvent.click(getByTestId('anchor-link'));
+
+    expect(container.innerHTML).toMatchSnapshot('expected html');
+    expect(HTMLElement.prototype.focus).toHaveBeenCalled();
+    expect(HTMLElement.prototype.scrollIntoView).toHaveBeenCalledWith({ behavior: 'smooth' });
+  });
+
+  it('should handle click event without an onClick callback', () => {
+    HTMLElement.prototype.scrollIntoView = vi.fn();
+    HTMLElement.prototype.focus = vi.fn();
+
+    const { container, getByTestId } = render(
+      <>
+        <AnchorLink anchorElementId="id">click here</AnchorLink>
+        <div id="id">Some content.</div>
+      </>,
+    );
+
+    fireEvent.click(getByTestId('anchor-link'));
+
+    expect(container.innerHTML).toMatchSnapshot('expected html');
+    expect(HTMLElement.prototype.focus).toHaveBeenCalled();
+    expect(HTMLElement.prototype.scrollIntoView).toHaveBeenCalledWith({ behavior: 'smooth' });
   });
 });
