2b2cacb6b4a65714493318541caeac522a747933
Dario Au <dario.au@hrsdc-rhdcc.gc.ca>
2025-01-28 07:44:20 -0700

    feat(frontend): progress component (#134)

--- frontend/app/utils/string-utils.ts
+++ frontend/app/utils/string-utils.ts
@@ -5,3 +5,12 @@ export function randomString(len: number, allowedChars = '0123456789abcdefghijkl
   const toRandomChar = () => allowedChars[Math.floor(Math.random() * allowedChars.length)];
   return Array(len).fill(undefined).map(toRandomChar).join('');
 }
+
+/**
+ * @param value - The whole number percentage (i.e. multiplied by 100) to be formatted
+ * @param locale - The Canadian locale to be used for formatting
+ * @returns - The number formatted as a percentage in the givenlocale
+ */
+export function formatPercent(value: number, locale: Language): string {
+  return Intl.NumberFormat(`${locale}-CA`, { style: 'percent' }).format(value / 100);
+}
