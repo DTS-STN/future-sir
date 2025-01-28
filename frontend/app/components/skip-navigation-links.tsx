e2b201e2d715d36068c809b56a704d86fa0c8c26
Greg Baker <48123208+gregory-j-baker@users.noreply.github.com>
2025-01-28 15:18:05 -0330

    fix(frontend): broken <SkipNavigationLinks> (#148)

--- frontend/app/components/skip-navigation-links.tsx
+++ frontend/app/components/skip-navigation-links.tsx
@@ -1,40 +1,26 @@
-import type { MouseEvent } from 'react';
-
 import { useTranslation } from 'react-i18next';
 
-import { ButtonLink } from '~/components/button-link';
-import { scrollAndFocusFromAnchorLink } from '~/utils/link-utils';
+import { AnchorLink } from '~/components/anchor-link';
 
 export function SkipNavigationLinks() {
   const { t } = useTranslation(['gcweb']);
 
-  /**
-   * handleOnSkipLinkClick is the click event handler for the anchor link.
-   * It prevents the default anchor link behavior, scrolls to and focuses
-   * on the target element specified by 'anchorElementId', and invokes
-   * the optional 'onClick' callback.
-   */
-  function handleOnSkipLinkClick(e: MouseEvent<HTMLAnchorElement>) {
-    e.preventDefault();
-    scrollAndFocusFromAnchorLink(e.currentTarget.href);
-  }
-
   return (
     <div id="skip-to-content">
-      {[
-        { to: '#wb-cont', children: t('gcweb:nav.skip-to-content') },
-        { to: '#wb-info', children: t('gcweb:nav.skip-to-about') },
-      ].map(({ to, children }) => (
-        <ButtonLink
-          key={to}
-          to={to}
-          onClick={handleOnSkipLinkClick}
-          variant="primary"
-          className="absolute z-10 mx-2 -translate-y-full transition-all focus:mt-2 focus:translate-y-0"
-        >
-          {children}
-        </ButtonLink>
-      ))}
+      <AnchorLink
+        anchorElementId="wb-cont"
+        className="absolute z-10 mx-2 -translate-y-full p-2 transition-all focus:mt-2 focus:translate-y-0"
+        data-testid="skip-to-content"
+      >
+        {t('gcweb:nav.skip-to-content')}
+      </AnchorLink>
+      <AnchorLink
+        anchorElementId="wb-info"
+        className="absolute z-10 mx-2 -translate-y-full p-2 transition-all focus:mt-2 focus:translate-y-0"
+        data-testid="skip-to-about"
+      >
+        {t('gcweb:nav.skip-to-about')}
+      </AnchorLink>
     </div>
   );
 }
