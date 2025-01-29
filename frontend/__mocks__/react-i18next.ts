8323b31de545b09b2d8e1d02c750989569121bba
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2025-01-28 20:40:09 -0330

    test(frontend): fix i18next automock

--- frontend/__mocks__/react-i18next.ts
+++ frontend/__mocks__/react-i18next.ts
@@ -1,6 +1,30 @@
 import { vi } from 'vitest';
 
+/**
+ * Transforms a key and optional options into a string representation.
+ *
+ * @param key - The key to be transformed.
+ * @param options - An optional object containing additional parameters to include in the transformation.
+ * @returns - A stringified representation of the key and options if options are provided; otherwise, the key itself.
+ *
+ * @example
+ * // without options
+ * tFunction('session-timeout');
+ * // Returns: 'session-timeout'
+ *
+ * @example
+ * // with options
+ * tFunction('session-timeout', { timeout: 300 });
+ * // Returns: '{"key":"session-timeout","options":{"timeout":300}}'
+ */
+function tFunction(key: string, options?: Record<string, unknown>) {
+  return options ? JSON.stringify({ key, options }) : key;
+}
+
+/**
+ * The vitest automock for react-i18next's useTranslation() hook.
+ */
 export const useTranslation = vi.fn(() => ({
-  t: (key: string) => key,
-  i18n: { getFixedT: vi.fn((lang: string) => (key: string) => key) },
+  i18n: { getFixedT: () => tFunction },
+  t: tFunction,
 }));
