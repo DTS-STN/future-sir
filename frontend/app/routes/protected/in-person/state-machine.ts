38f50ac9b0ad7370f2594a0512c628264ecffd38
Greg Baker <48123208+gregory-j-baker@users.noreply.github.com>
2025-01-30 13:17:27 -0330

    feat(frontend): add XState and initial FSM+tests (#160)
    * npm install xstate
    
    * Add initial state machine + tests

---
+++ frontend/app/routes/protected/in-person/state-machine.ts
@@ -0,0 +1,95 @@
+import { setup } from 'xstate';
+
+type Event = { type: 'prev' } | { type: 'next' } | { type: 'cancel' };
+
+export const machine = setup({
+  types: {
+    events: {} as Event,
+  },
+}).createMachine({
+  id: '(in-person-machine)',
+  initial: 'start',
+  states: {
+    'start': {
+      on: {
+        next: { target: 'privacy-statement' },
+      },
+    },
+    'privacy-statement': {
+      on: {
+        prev: { target: 'start' },
+        next: { target: 'request-details' },
+        cancel: { target: 'start' },
+      },
+    },
+    'request-details': {
+      on: {
+        prev: { target: 'privacy-statement' },
+        next: { target: 'primary-docs' },
+        cancel: { target: 'start' },
+      },
+    },
+    'primary-docs': {
+      on: {
+        prev: { target: 'request-details' },
+        next: { target: 'secondary-docs' },
+        cancel: { target: 'start' },
+      },
+    },
+    'secondary-docs': {
+      on: {
+        prev: { target: 'primary-docs' },
+        next: { target: 'name-info' },
+        cancel: { target: 'start' },
+      },
+    },
+    'name-info': {
+      on: {
+        prev: { target: 'secondary-docs' },
+        next: { target: 'personal-info' },
+        cancel: { target: 'start' },
+      },
+    },
+    'personal-info': {
+      on: {
+        prev: { target: 'name-info' },
+        next: { target: 'birth-info' },
+        cancel: { target: 'start' },
+      },
+    },
+    'birth-info': {
+      on: {
+        prev: { target: 'personal-info' },
+        next: { target: 'parent-info' },
+        cancel: { target: 'start' },
+      },
+    },
+    'parent-info': {
+      on: {
+        prev: { target: 'birth-info' },
+        next: { target: 'previous-sin-info' },
+        cancel: { target: 'start' },
+      },
+    },
+    'previous-sin-info': {
+      on: {
+        prev: { target: 'parent-info' },
+        next: { target: 'contact-info' },
+        cancel: { target: 'start' },
+      },
+    },
+    'contact-info': {
+      on: {
+        prev: { target: 'previous-sin-info' },
+        next: { target: 'review' },
+        cancel: { target: 'start' },
+      },
+    },
+    'review': {
+      on: {
+        cancel: { target: 'start' },
+        // TODO --- rest of events
+      },
+    },
+  },
+});
