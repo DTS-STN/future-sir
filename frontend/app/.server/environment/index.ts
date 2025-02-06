86b8a2682000b93f65de4b8eafb309efa4bf4d9f
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2025-02-06 10:43:37 -0400

    refactor(frontend): remove Zod utilities

--- frontend/app/.server/environment/index.ts
+++ frontend/app/.server/environment/index.ts
@@ -5,7 +5,7 @@ import type { Client } from '~/.server/environment/client';
 import { client } from '~/.server/environment/client';
 import type { Server } from '~/.server/environment/server';
 import { server } from '~/.server/environment/server';
-import * as ValidationUtils from '~/utils/validation-utils';
+import { preprocess } from '~/utils/validation-utils';
 
 export { defaults as clientDefaults } from '~/.server/environment/client';
 export { defaults as serverDefaults } from '~/.server/environment/server';
@@ -13,7 +13,7 @@ export { defaults as serverDefaults } from '~/.server/environment/server';
 export type ClientEnvironment = Client;
 export type ServerEnvironment = Server;
 
-const processed = ValidationUtils.preprocess(process.env);
+const processed = preprocess(process.env);
 const isProduction = processed.NODE_ENV === 'production';
 
 export const clientEnvironment = v.parse(client, { ...processed, isProduction });
