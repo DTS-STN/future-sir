d1bd7a187b7c13461c2baafce77deb27ffc3c3e5
Greg Baker <48123208+gregory-j-baker@users.noreply.github.com>
2025-02-20 10:10:34 -0330

    feat(frontend): add `isValidTimeZone()` function (#238)

--- frontend/app/utils/date-utils.ts
+++ frontend/app/utils/date-utils.ts
@@ -51,3 +51,21 @@ export function getLocalizedMonths(
     text: formatter.format(Date.UTC(0, i, 1)), // formatted month name
   }));
 }
+
+/**
+ * Checks if a given string is a valid time zone.
+ *
+ * see: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+ *
+ * @param timeZone - The time zone string to validate.
+ * @returns `true` if the time zone is valid, `false` if it is not
+ */
+export function isValidTimeZone(timeZone: string): boolean {
+  try {
+    Intl.DateTimeFormat(undefined, { timeZone });
+  } catch {
+    return false;
+  }
+
+  return true;
+}
