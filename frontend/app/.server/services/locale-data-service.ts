923c4928fb86515c175dfef0b5cd8a59226a653a
Frank Basham <frankabasham@gmail.com>
2025-02-21 09:57:08 -0500

    add province-territory-state.json and service methods

--- frontend/app/.server/services/locale-data-service.ts
+++ frontend/app/.server/services/locale-data-service.ts
@@ -1,4 +1,5 @@
 import countryData from '~/.server/resources/countries.json';
+import provinceTerritoryStateData from '~/.server/resources/province-territory-state.json';
 
 type Country = Readonly<{
   id: string;
@@ -23,5 +24,35 @@ export function getLocalizedCountries(locale: Language = 'en'): readonly Localiz
   return getCountries().map((country) => ({
     id: country.id,
     name: country[locale === 'en' ? 'nameEn' : 'nameFr'],
+  }));
+}
+
+type ProvinceTerritoryState = Readonly<{
+  id: string;
+  countryId: string;
+  nameEn: string;
+  nameFr: string;
+}>;
+
+export function getProvincesTerritoriesStates(): readonly ProvinceTerritoryState[] {
+  return provinceTerritoryStateData.value.map((region) => ({
+    id: region.esdc_provinceterritorystateid,
+    countryId: region._esdc_countryid_value,
+    nameEn: region.esdc_nameenglish,
+    nameFr: region.esdc_namefrench,
+  }));
+}
+
+type LocalizedProvinceTerritoryState = Readonly<{
+  id: string;
+  countryId: string;
+  name: string;
+}>;
+
+export function getLocalizedProvincesTerritoriesStates(locale: Language = 'en'): readonly LocalizedProvinceTerritoryState[] {
+  return getProvincesTerritoriesStates().map((region) => ({
+    id: region.id,
+    countryId: region.countryId,
+    name: region[locale === 'en' ? 'nameEn' : 'nameFr'],
   }));
 }
