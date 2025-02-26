cdbfc2a51f5224cc6a364225138732d7918202a7
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2025-02-26 16:33:42 -0400

    feat(frontend): translate valibate issues

--- frontend/app/utils/validation-utils.ts
+++ frontend/app/utils/validation-utils.ts
@@ -1,3 +1,9 @@
+import type { FlatNamespace, KeyPrefix } from 'i18next';
+import { getDotPath } from 'valibot';
+import type { BaseIssue, BaseSchema, BaseSchemaAsync, InferIssue } from 'valibot';
+
+import { getTranslation } from '~/i18n-config.server';
+
 /**
  * Preprocesses validation input.
  *
@@ -13,4 +19,23 @@ export function preprocess<K extends string | number | symbol, T>(data: Record<K
     .map(([key, val]) => [key, val === '' ? undefined : val]);
 
   return Object.fromEntries(processedEntries);
+}
+
+export async function translateIssues<
+  TSchema extends BaseSchema<unknown, unknown, BaseIssue<unknown>> | BaseSchemaAsync<unknown, unknown, BaseIssue<unknown>>,
+  TIssues extends [InferIssue<TSchema>, ...InferIssue<TSchema>[]],
+  TLanguageOrRequest extends Language | Request,
+  TNs extends FlatNamespace,
+  TKPrefix extends KeyPrefix<TNs> = undefined,
+>(issues: TIssues, LanguageOrRequest: TLanguageOrRequest, ns: TNs, keyPrefix?: TKPrefix): Promise<TIssues> {
+  const { t } = await getTranslation(LanguageOrRequest, ns, keyPrefix);
+  return issues.map((issue) => {
+    const issueType = issue.type;
+    const dotPath = getDotPath(issue);
+    const i18nKey = [dotPath, issueType].filter(Boolean).join('.');
+    const defaultValue = issue.message;
+    const requirement = issue.requirement;
+    const message = t(i18nKey, defaultValue, { requirement }) as string;
+    return { ...issue, message };
+  }) as TIssues;
 }
