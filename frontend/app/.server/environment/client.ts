d674a18376d2d4bea36b2f8e94d0d1887a21d416
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2025-03-05 13:54:12 -0330

    fix(frontend): move BASE_TIMEZONE to client config

--- frontend/app/.server/environment/client.ts
+++ frontend/app/.server/environment/client.ts
@@ -2,10 +2,12 @@ import * as v from 'valibot';
 
 import { stringToBooleanSchema } from '~/.server/validation/string-to-boolean-schema';
 import { stringToIntegerSchema } from '~/.server/validation/string-to-integer-schema';
+import { isValidTimeZone } from '~/utils/date-utils';
 
 export type Client = Readonly<v.InferOutput<typeof client>>;
 
 export const defaults = {
+  BASE_TIMEZONE: 'Canada/Eastern',
   BUILD_DATE: '1970-01-01T00:00:00.000Z',
   BUILD_ID: '000000',
   BUILD_REVISION: '00000000',
@@ -22,6 +24,7 @@ export const defaults = {
  * ⚠️ IMPORTANT: DO NOT PUT SENSITIVE CONFIGURATIONS HERE ⚠️
  */
 export const client = v.object({
+  BASE_TIMEZONE: v.optional(v.pipe(v.string(), v.check(isValidTimeZone)), defaults.BASE_TIMEZONE),
   BUILD_DATE: v.optional(v.string(), defaults.BUILD_DATE),
   BUILD_ID: v.optional(v.string(), defaults.BUILD_ID),
   BUILD_REVISION: v.optional(v.string(), defaults.BUILD_REVISION),
