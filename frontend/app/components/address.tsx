b1071b668c247b642291e16f12a4a0f195dee1cb
Greg Baker <48123208+gregory-j-baker@users.noreply.github.com>
2025-03-07 09:41:04 -0330

    refactor(frontend): fixup `<Address>` types (#356)

--- frontend/app/components/address.tsx
+++ frontend/app/components/address.tsx
@@ -3,24 +3,9 @@ import type { ComponentProps, JSX } from 'react';
 import { formatAddress } from '~/utils/string-utils';
 import { cn } from '~/utils/tailwind-utils';
 
-type Address = {
-  addressLine1?: string;
-  addressLine2?: string;
-  city?: string;
-  postalZipCode?: string;
-  provinceState?: string;
-  country: string;
-};
-
 type AddressProps = ComponentProps<'address'> & {
-  address: Address;
-  /**
-   * The format of the address
-   *
-   * - `standard`: The standard address format, with the address line, city, province/state, postal/zip code, and country.
-   * - `alternative`: An alternative address format, with the address line, city, province/state, postal/zip code, and country on separate lines.
-   */
-  format?: 'standard' | 'alternative';
+  address: Parameters<typeof formatAddress>[0];
+  format?: Parameters<typeof formatAddress>[1];
 };
 
 export function Address({ address, format, className, ...rest }: AddressProps): JSX.Element {
