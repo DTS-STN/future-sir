f6d0f06ca4b073131026a8fbc5377b28db0a60c4
Greg Baker <48123208+gregory-j-baker@users.noreply.github.com>
2025-03-10 08:23:36 -0230

    refactor(frontend): rename Interop API environment variables for clarity (#368)

--- frontend/app/.server/environment/interop-api.ts
+++ frontend/app/.server/environment/interop-api.ts
@@ -5,11 +5,14 @@ import { Redacted } from '~/.server/utils/security-utils';
 export type InteropApi = Readonly<v.InferOutput<typeof interopApi>>;
 
 export const defaults = {
-  INTEROP_API_AUTH_HEADER: 'Ocp-Apim-Subscription-Key 00000000000000000000000000000000',
-  INTEROP_API_BASE_URL: 'http://localhost:3000/api',
+  INTEROP_SIN_REG_API_AUTH_HEADER: 'Ocp-Apim-Subscription-Key 00000000000000000000000000000000',
+  INTEROP_SIN_REG_API_BASE_URL: 'http://localhost:3000/api',
 } as const;
 
 export const interopApi = v.object({
-  INTEROP_API_AUTH_HEADER: v.pipe(v.optional(v.string(), defaults.INTEROP_API_AUTH_HEADER), v.transform(Redacted.make)),
-  INTEROP_API_BASE_URL: v.optional(v.string(), defaults.INTEROP_API_BASE_URL),
+  INTEROP_SIN_REG_API_AUTH_HEADER: v.pipe(
+    v.optional(v.string(), defaults.INTEROP_SIN_REG_API_AUTH_HEADER),
+    v.transform(Redacted.make),
+  ),
+  INTEROP_SIN_REG_API_BASE_URL: v.optional(v.string(), defaults.INTEROP_SIN_REG_API_BASE_URL),
 });
